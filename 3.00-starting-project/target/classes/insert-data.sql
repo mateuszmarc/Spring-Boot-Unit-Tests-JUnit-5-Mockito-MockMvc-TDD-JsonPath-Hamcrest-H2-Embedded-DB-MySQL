34ad1ca08e9c3004fd25aef98f77915c5b1cd8e5
mateuszmarc <mmarckiewicz@gmail.com>
2024-12-12 08:40:29 +0000

    Database integratin testing practice

---
+++ 3.00-starting-project/target/classes/insert-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO student(id, firstname, lastname, email_address)
+VALUES (11, 'Student', 'One', 'one@gmail.com'),
+       (12, 'Student', 'Two', 'two@gmail.com'),
+       (13, 'Student', 'Three', 'three@gmail.com'),
+       (14, 'Student', 'Four', 'four@gmail.com');
